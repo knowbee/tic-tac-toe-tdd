426baa77d2b22be442cc728715c6c7056b9fe196
Anselme Mucunguzi <mucunguzia@gmail.com>
2021-05-24 22:03:32 +0200

    Use proper dependency injection in GameSession

--- game_session.py
+++ game_session.py
@@ -1,42 +1,36 @@
 from core.game_type import HumanVsHuman
-import core
+from core import Game, GameDisplay
+
 
 class GameSession:
-    def __init__(self):
-        self.human_vs_human =  HumanVsHuman()
-        self.game = core.Game()
-        self.game_type = None
-        self.first_player = None
+    def __init__(self, human_vs_human: HumanVsHuman, game_diplay: GameDisplay):
+        self.human_vs_human = human_vs_human
+        self.game_display: GameDisplay = game_diplay
 
-    def start_game_session(self):
-        self.handle_game_type()
-        self.first_player: str = self.get_first_player()
-        if self.game_type == 0:
-            self.handle_human_players_symbols()
-            self.human_vs_human.play()
-            return
-    
-    def handle_game_type(self):
-        self.game_type = core.GameDisplay.get_game_type()
-        
-    @classmethod
-    def get_first_player(cls) -> str:
-        print("Who plays first? X or O")
-        first_player = input()
-        valid_input = ["O", "X"]
-        if not first_player in valid_input:
-            return cls.get_first_player(cls)
-        else:
-            return first_player
-    
-    def handle_human_players_symbols(self):
-        if self.first_player == "X":
+    def get_match(self):
+        first_player: str = self.game_display.get_first_player()
+        self.set_game_players(first_player)
+
+        if self.game_display.get_game_type() == 0:
+            return self.human_vs_human
+
+    def set_game_players(self, first_player: str) -> Game:
+        self.set_player_symbols(first_player)
+        self._initiate_current_player_symbol(first_player)
+
+    def set_player_symbols(self, first_player: str) -> None:
+        if first_player == "X":
             self.human_vs_human.playerTwo.symbol = "O"
             self.human_vs_human.playerOne.symbol = "X"
         else:
             self.human_vs_human.playerTwo.symbol = "X"
             self.human_vs_human.playerOne.symbol = "O"
 
+    def _initiate_current_player_symbol(self, first_player: str) -> None:
+        self.human_vs_human.current_player_symbol = first_player
+
+
 if __name__ == "__main__":
-    game = GameSession()
-    game.start_game_session()
+    game_session = GameSession(HumanVsHuman(), GameDisplay)
+    match = game_session.get_match()
+    match.play()
--- tests/test_game_session.py
+++ tests/test_game_session.py
@@ -2,27 +2,63 @@ import unittest
 from game_session import GameSession
 import core
 from core.game_type import HumanVsHuman
+from cli import GameDisplay
+
+
+class MockGameDiplay(object):
+    def get_game_type(self) -> int:
+        return 0
+
+    def get_first_player(self) -> str:
+        return 'X'
+
 
 class TestGameSession(unittest.TestCase):
-   def setUp(self):
-     self.game_session: GameSession = GameSession()
-     self.game: core.Game = core.Game()
-     self.humanPlayer = HumanVsHuman()
+    def setUp(self):
+        self.game_display = MockGameDiplay()
+        self.game_session: GameSession = GameSession(
+            HumanVsHuman(), self.game_display)
+        self.game: core.Game = core.Game()
+        self.humanPlayer = HumanVsHuman()
 
-   def test_when_first_player_is_O_player_one_symbol_should_be_O(self):
-      self.game_session.first_player = "O"
-      self.game_session.handle_human_players_symbols()
-      self.assertEqual(self.game_session.human_vs_human.playerOne.symbol, "O", "If the first player is O, player one symbol should be O")
-      self.assertEqual(self.game_session.human_vs_human.playerTwo.symbol, "X", "If the first player is X, player one symbol should be X")
-   
-   def test_when_first_player_is_X_player_one_symbol_should_be_X(self):
-      self.game_session.first_player = "X"
-      self.game_session.handle_human_players_symbols()
-      self.assertEqual(self.game_session.human_vs_human.playerOne.symbol, "X", "If the first player is X, player one symbol should be X")
-      self.assertEqual(self.game_session.human_vs_human.playerTwo.symbol, "O", "If the first player is O, player one symbol should be O")
+    def test_when_first_player_is_O_player_one_symbol_should_be_O(self):
+        self.game_session.set_player_symbols("O")
+        self.assertEqual(self.game_session.human_vs_human.playerOne.symbol,
+                         "O", "If the first player is O, player one symbol should be O")
+        self.assertEqual(self.game_session.human_vs_human.playerTwo.symbol,
+                         "X", "If the first player is X, player one symbol should be X")
 
-   def test_if_first_player_X_current_player_symbol_should_be_X_when_game_starts(self):
-      first_player = "X"
-      self.game.current_player_symbol = first_player
-      self.game_session.handle_human_players_symbols()
-      self.assertEqual(self.game.current_player_symbol, "X", "If first player is X, current player symbol should be X")
+    def test_when_first_player_is_X_player_one_symbol_should_be_X(self):
+        self.game_session.set_player_symbols("X")
+        self.assertEqual(self.game_session.human_vs_human.playerOne.symbol,
+                         "X", "If the first player is X, player one symbol should be X")
+        self.assertEqual(self.game_session.human_vs_human.playerTwo.symbol,
+                         "O", "If the first player is O, player one symbol should be O")
+
+    def test_if_first_player_X_current_player_symbol_should_be_X_when_game_starts(self):
+        first_player = "X"
+        self.game.current_player_symbol = first_player
+        self.game_session.set_player_symbols(first_player)
+        self.assertEqual(self.game.current_player_symbol, "X",
+                         "If first player is X, current player symbol should be X")
+
+    def test_set_game_session_sets_players_symbols(self):
+        self.game_session.set_game_players('X')
+
+        self.assertEqual('X', self.game_session.human_vs_human.playerOne.symbol,
+                         'Incorrect symbol for first player.')
+        self.assertEqual('O', self.game_session.human_vs_human.playerTwo.symbol,
+                         'Incorrect symbol for second player.')
+
+    def test_game_should_have_first_player_as_current_player_symbol(self):
+        self.game_session.set_game_players('X')
+
+        self.assertEqual('X', self.game_session.human_vs_human.current_player_symbol,
+                         'Incorrect current player symbol.')
+
+    def test_get_match_should_return_game_with_players_set(self):
+        game: HumanVsHuman = self.game_session.get_match()
+
+        self.assertEqual('X', game.playerOne.symbol, 'Incorrect first player')
+        self.assertEqual('O', game.playerTwo.symbol,
+                         'Incorrect second player')
