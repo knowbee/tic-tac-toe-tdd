0aed5aa668684ba7352df64b66122f31275070ba
knowbee <knowbeeinc@gmail.com>
2021-05-27 22:09:41 +0200

    refactor

--- game_session.py
+++ game_session.py
@@ -2,22 +2,24 @@ from core.game_type import HumanVsHuman, HumanVsComputer
 from core import Game, GameDisplay
 
 class GameSession:
-    def __init__(self, human_vs_human: HumanVsHuman, human_vs_computer: HumanVsComputer, game_display: GameDisplay):
-        self.human_vs_human = human_vs_human
-        self.human_vs_computer = human_vs_computer
+    def __init__(self, game_display: GameDisplay):
         self.game_display: GameDisplay = game_display
 
-    def get_match(self):
-        if self.game_display.get_game_type() == 0:
-            first_player: str = self.game_display.get_first_player()
-            self.human_vs_human.set_game_players(first_player)
-            return self.human_vs_human
+    @classmethod
+    def get_game(cls, game_type):
+        if game_type == 0:
+            return HumanVsHuman()
         else:
-            first_player: str = self.game_display.get_first_player()
-            self.human_vs_computer.set_game_players(first_player)
-            return self.human_vs_computer
+            return HumanVsComputer()
+
+    def get_match(self):
+        game_type = self.game_display.get_game_type()
+        game = self.get_game(game_type)
+        first_player: str = self.game_display.get_first_player()
+        game.set_game_players(first_player)
+        return game
 
 if __name__ == "__main__":
-    game_session = GameSession(HumanVsHuman(), HumanVsComputer(),  GameDisplay)
+    game_session = GameSession(GameDisplay)
     match = game_session.get_match()
     match.play()
