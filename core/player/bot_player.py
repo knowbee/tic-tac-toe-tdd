3f5145812fc7a94ab3042a76da9a105c3a1b6b2f
knowbee <knowbeeinc@gmail.com>
2021-06-16 23:02:25 +0200

    It works

--- core/player/bot_player.py
+++ core/player/bot_player.py
@@ -18,66 +18,62 @@ class BotPlayer(Player):
         else:
             self.opponent_symbol = "X"
 
-    def alpha_beta(self, board, is_max, alpha, beta):
+    def alpha_beta(self, board, alpha, beta):
         cloned_board = copy.deepcopy(board)
+        return self.maximizer_alpha_beta(cloned_board, alpha, beta)
 
-        if is_max:
-            return self.maximizer_alpha(cloned_board, alpha, beta)
-        else:
-            return self.minimizer_alpha(cloned_board, alpha, beta)
-
-    def minimax(self, board: Board, is_max: bool) -> Tuple:
+    def minimax(self, board: Board, is_max) -> Tuple:
 
         cloned_board = copy.deepcopy(board)
         if is_max:
             return self.maximizer(cloned_board)
         return self.minimizer(cloned_board)
 
-    def minimizer_alpha(self, board: Board, alpha, beta) -> Tuple:
-        best_score: Optional[int] = None
+    def minimizer_alpha_beta(self, board: Board, alpha, beta) -> Tuple:
+        best_score: int = 100
         best_move: Optional[int] = None
         cloned_board = copy.deepcopy(board)
 
         for move in cloned_board.get_available_spots():
             cloned_board.set_spot(int(move), self.symbol)
             if self.game_state.finished(cloned_board):
-                score = self.get_score(cloned_board)
+                return self.get_score(cloned_board), move
             else:
-                score, move_position = self.alpha_beta(cloned_board, True, alpha, beta)
-            beta = min(alpha, score)
-            if best_score == None or score > best_score:
-                best_score = score
-                best_move = move
+                score, move_position = self.maximizer_alpha_beta(cloned_board, alpha, beta)
+                if score < best_score:
+                    best_move = move
+                    best_score = score
 
-            if beta <= alpha:
-                break
-
-            cloned_board = copy.deepcopy(board)
+                if score <= alpha:
+                    return score, move
+                if score < beta:
+                    beta = score
+                cloned_board = copy.deepcopy(board)
 
         return best_score, best_move
 
-    def maximizer_alpha(self, board: Board, alpha, beta):
+    def maximizer_alpha_beta(self, board: Board, alpha, beta):
 
-        best_score: Optional[int] = None
+        best_score: int = -100
         best_move: Optional[int] = None
         cloned_board = copy.deepcopy(board)
 
         for move in cloned_board.get_available_spots():
             cloned_board.set_spot(int(move), self.opponent_symbol)
             if self.game_state.finished(cloned_board):
-                score = self.get_score(cloned_board)
+                return self.get_score(cloned_board), int(move)
             else:
-                score, move_position = self.alpha_beta(cloned_board, False, alpha, beta)
-            alpha = max(alpha, score)
+                score, move_position = self.minimizer_alpha_beta(cloned_board, alpha, beta)
+                if score > best_score:
+                    best_move = move
+                    best_score = score
 
-            if best_score == None or score < best_score:
-                best_score = score
-                best_move = move
+                if score >= beta:
+                    return best_score, move
 
-            if beta <= alpha:
-                break
-
-            cloned_board = copy.deepcopy(board)
+                if score > alpha:
+                    alpha = score
+                cloned_board = copy.deepcopy(board)
         return best_score, best_move
 
     def minimizer(self, board):
@@ -131,7 +127,7 @@ class BotPlayer(Player):
     def play(self, board: Board, **kwargs) -> int:
         self.set_up()
         score, move = self.minimax(board, False)
-        # score, move = self.alpha_beta(board, False, -100, 100)
+        # score, move = self.alpha_beta(board, -100, 100)
         board.set_spot(int(move), self.symbol)
         return move
 
