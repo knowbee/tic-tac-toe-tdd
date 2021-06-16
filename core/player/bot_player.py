54c0d5bc218a48283e2d142bfaaf329b0a748b75
knowbee <knowbeeinc@gmail.com>
2021-06-16 21:00:16 +0200

    alpha beta not working as expected

--- core/player/bot_player.py
+++ core/player/bot_player.py
@@ -12,95 +12,126 @@ class BotPlayer(Player):
         self.game_state = GameState()
         self.opponent_symbol = None
 
-    def get_best_move(self, board):
-        if board.grid[4] != "X" and board.grid[4] != "O":
-            return 4
-        else:
-            score, move = self.alphaBetaMove(board)
-            return move
-
-    def alphaBetaMove(self, board: Board):
-        cloned_board = copy.deepcopy(board)
-
-        alpha = -1000
-        beta = 1000
-        score = -1000
-        move = None
-        opponent = None
-
-        if self.symbol == "O":
-            self.opponent_symbol = "X"
+    def set_up(self):
         if self.symbol == "X":
             self.opponent_symbol = "O"
+        else:
+            self.opponent_symbol = "X"
 
-        for m in cloned_board.get_available_spots():
-            cloned_board.set_spot(int(m), self.opponent_symbol)
-
-            if self.game_state.finished(cloned_board):
-                return self.get_score(cloned_board), m
-            else:
-                current_score = self.minimizer(cloned_board, alpha, beta)
-                cloned_board = copy.deepcopy(board)
-
-                if current_score > score:
-                    score = current_score
-                    move = m
-                alpha = max(score, alpha)
-
-        return score, move
-
-    def minimizer(self, board: Board, alpha, beta) -> Tuple:
-        cloned_board = copy.deepcopy(board)
-        best_score: int = -1000
-        best_move: Optional[int] = None
-        opponent = None
-
-        for move in cloned_board.get_available_spots():
-            cloned_board.set_spot(int(move), self.opponent_symbol)
-            if self.game_state.finished(cloned_board):
-                return self.get_score(cloned_board)
-            else:
-                score = self.maximizer(cloned_board, alpha, beta)
-            cloned_board = copy.deepcopy(board)
-
-            if score <= alpha:
-                best_score = score
-                return score
-
-            beta = min(beta, score)
-        return best_score
-
-    def maximizer(self, board: Board, alpha, beta) -> Tuple:
+    def alpha_beta(self, board, is_max, alpha, beta):
         cloned_board = copy.deepcopy(board)
 
-        best_score: int = 1000
+        if is_max:
+            return self.maximizer_alpha(cloned_board, alpha, beta)
+        else:
+            return self.minimizer_alpha(cloned_board, alpha, beta)
+
+    def minimax(self, board: Board, is_max: bool) -> Tuple:
+
+        cloned_board = copy.deepcopy(board)
+        if is_max:
+            return self.maximizer(cloned_board)
+        return self.minimizer(cloned_board)
+
+    def minimizer_alpha(self, board: Board, alpha, beta) -> Tuple:
+        best_score: Optional[int] = None
         best_move: Optional[int] = None
+        cloned_board = copy.deepcopy(board)
 
         for move in cloned_board.get_available_spots():
             cloned_board.set_spot(int(move), self.symbol)
             if self.game_state.finished(cloned_board):
-                return self.get_score(cloned_board)
+                score = self.get_score(cloned_board)
             else:
-                score = self.minimizer(cloned_board, alpha, beta)
+                score, move_position = self.alpha_beta(cloned_board, True, alpha, beta)
+            beta = min(alpha, score)
+            if best_score == None or score > best_score:
+                best_score = score
+                best_move = move
+
+            if beta <= alpha:
+                break
+
             cloned_board = copy.deepcopy(board)
 
-            if score >= beta:
-                best_score = score
-                return score
+        return best_score, best_move
+
+    def maximizer_alpha(self, board: Board, alpha, beta):
+
+        best_score: Optional[int] = None
+        best_move: Optional[int] = None
+        cloned_board = copy.deepcopy(board)
+
+        for move in cloned_board.get_available_spots():
+            cloned_board.set_spot(int(move), self.opponent_symbol)
+            if self.game_state.finished(cloned_board):
+                score = self.get_score(cloned_board)
+            else:
+                score, move_position = self.alpha_beta(cloned_board, False, alpha, beta)
             alpha = max(alpha, score)
-        return best_score
+
+            if best_score == None or score < best_score:
+                best_score = score
+                best_move = move
+
+            if beta <= alpha:
+                break
+
+            cloned_board = copy.deepcopy(board)
+        return best_score, best_move
+
+    def minimizer(self, board):
+        best_score: Optional[int] = None
+        best_move: Optional[int] = None
+        cloned_board = copy.deepcopy(board)
+
+        for move in cloned_board.get_available_spots():
+            cloned_board.set_spot(int(move), self.symbol)
+            if self.game_state.finished(cloned_board):
+                score = self.get_score(cloned_board)
+            else:
+                score, move_position = self.minimax(cloned_board, True)
+            cloned_board = copy.deepcopy(board)
+
+            if best_score == None or score > best_score:
+                best_score = score
+                best_move = move
+
+        return best_score, best_move
+
+    def maximizer(self, board):
+        best_score: Optional[int] = None
+        best_move: Optional[int] = None
+        cloned_board = copy.deepcopy(board)
+
+        for move in cloned_board.get_available_spots():
+            cloned_board.set_spot(int(move), self.opponent_symbol)
+            if self.game_state.finished(cloned_board):
+                score = self.get_score(cloned_board)
+            else:
+                score, move_position = self.minimax(cloned_board, False)
+            cloned_board = copy.deepcopy(board)
+
+            if best_score == None or score < best_score:
+                best_score = score
+                best_move = move
+
+        return best_score, best_move
 
     def get_score(self, board: Board) -> int:
-        winner = self.game_state.get_winner(board)
-        if winner == self.symbol:
-            return 1
-        elif winner == self.opponent_symbol:
-            return -1
+        if self.game_state.finished(board):
+            winner = self.game_state.get_winner(board)
+            if winner == self.symbol:
+                return 1
+            elif winner == self.opponent_symbol:
+                return -1
 
         return 0
 
     def play(self, board: Board, **kwargs) -> int:
-        move = self.get_best_move(board)
+        self.set_up()
+        score, move = self.minimax(board, False)
+        # score, move = self.alpha_beta(board, False, -100, 100)
         board.set_spot(int(move), self.symbol)
         return move
 
--- tests/test_bot_player.py
+++ tests/test_bot_player.py
@@ -30,47 +30,40 @@ class TestBot(unittest.TestCase):
     def test_BotPlayer_get_score_for_empty_board_should_be_0(self):
         self.assertEqual(self.bot_player.get_score(self.board), 0)
 
-    # def test_BotPlayer_maximizer_should_return_tuple_of_None_when_game_is_a_tie(self):
-    #     self.board.grid = [
-    #         "X", "X", "O",
-    #         "O", "O", "X",
-    #         "X", "X", "X"]
-    #     self.assertEqual(self.bot_player.maximizer(self.board), (None, None))
+    def test_BotPlayer_maximizer_should_return_tuple_of_None_when_game_is_a_tie(self):
+        self.board.grid = ["X", "X", "O", "O", "O", "X", "X", "X", "X"]
+        self.assertEqual(self.bot_player.maximizer(self.board), (None, None))
 
-    # def test_BotPlayer_minimizer_should_return_tuple_of_None_when_game_is_a_win(self):
-    #     self.board.grid = [
-    #         "O", "X", "X",
-    #         "X", "O", "O",
-    #         "X", "O", "O"]
-    #     self.assertEqual(self.bot_player.minimizer(self.board), (None, None))
+    def test_BotPlayer_minimizer_should_return_tuple_of_None_when_game_is_a_win(self):
+        self.board.grid = ["O", "X", "X", "X", "O", "O", "X", "O", "O"]
+        self.assertEqual(self.bot_player.minimizer(self.board), (None, None))
 
-    # def test_BotPlayer_maximizer_should_return_1_and_5_as_best_winning_move_for_BotPlayer(self):
-    #     self.board.grid = [
-    #         "X", "O", "X",
-    #         "O", "X", "5",
-    #         "O", "7", "8"]
-    #     self.assertEqual(self.bot_player.maximizer(self.board), (1, "5"))
+    def test_BotPlayer_maximizer_should_return_1_and_8_as_best_winning_move_for_BotPlayer(self):
+        self.board.grid = ["X", "O", "X",
+                           "O", "X", "O",
+                           "X", "O", "8"]
+        self.assertEqual(self.bot_player.maximizer(self.board), (1, "8"))
 
-    # def test_BotPlayer_maximizer_should_return_1_and_6_as_best_winning_move_for_BotPlayer(self):
-    #     self.board.grid = [
-    #         "X", "O", "O",
-    #         "X", "O", "X",
-    #         "6", "X", "8"]
-    #     self.assertEqual(self.bot_player.maximizer(self.board), (1, "6"))
+    def test_BotPlayer_maximizer_should_return_minus_1_and_6_as_best_winning_move_for_BotPlayer(self):
+        self.board.grid = [
+            "O", "O", "X",
+            "O", "X", "O",
+            "6", "O", "8"]
+        self.assertEqual(self.bot_player.maximizer(self.board), (-1, "6"))
 
-    # def test_BotPlayer_minimizer_should_return_minus_1_and_0_as_best_winning_move_for_HumanPlayer(self):
-    #     self.board.grid = [
-    #         "0", "O", "X",
-    #         "O", "X", "5",
-    #         "O", "7", "8"]
-    #     self.assertEqual(self.bot_player.minimizer(self.board), (-1, "0"))
+    def test_BotPlayer_minimizer_should_return_minus_1_and_0_as_best_winning_move_for_HumanPlayer(self):
+        self.board.grid = [
+            "0", "X", "X",
+            "O", "X", "5",
+            "O", "7", "8"]
+        self.assertEqual(self.bot_player.minimizer(self.board), (1, "0"))
 
-    # def test_BotPlayer_minimizer_should_return_minus_1_and_2_as_best_winning_move_for_HumanPlayer(self):
-    #     self.board.grid = [
-    #         "O", "O", "2",
-    #         "3", "X", "X",
-    #         "O", "7", "O"]
-    #     self.assertEqual(self.bot_player.minimizer(self.board), (-1, "2"))
+    def test_BotPlayer_minimizer_should_return_1_and_3_as_best_winning_move_for_HumanPlayer(self):
+        self.board.grid = [
+            "O", "O", "2",
+            "3", "X", "X",
+            "O", "7", "O"]
+        self.assertEqual(self.bot_player.minimizer(self.board), (1, "3"))
 
 
 if __name__ == "__main__":
