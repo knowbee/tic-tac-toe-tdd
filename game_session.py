7c0a58df98b78bc8793da2dab810d617c98ef7df
knowbee <knowbeeinc@gmail.com>
2021-06-23 19:18:10 +0200

    refactor game_session

--- game_session.py
+++ game_session.py
@@ -1,5 +1,8 @@
-from core import Game, GameDisplay
 from core.player import HumanPlayer, BotPlayer
+from cli import GameDisplay
+from cli.game import Game
+from core.board import Board
+from core.game_state import GameState
 
 
 class GameSession:
@@ -9,9 +12,21 @@ class GameSession:
     @classmethod
     def get_game(cls, game_type: int):
         if game_type == 0:
-            return Game(player_one=HumanPlayer(), player_two=HumanPlayer(), game_display=GameDisplay())
+            return Game(
+                player_one=HumanPlayer(),
+                player_two=HumanPlayer(),
+                game_display=GameDisplay(),
+                board=Board(GameDisplay.get_board_size()),
+                game_state=GameState(),
+            )
         else:
-            return Game(player_one=HumanPlayer(), player_two=BotPlayer(), game_display=GameDisplay())
+            return Game(
+                player_one=HumanPlayer(),
+                player_two=BotPlayer(),
+                game_display=GameDisplay(),
+                board=Board(GameDisplay.get_board_size()),
+                game_state=GameState(),
+            )
 
     def get_match(self):
         game_type: int = self.game_display.get_game_type(self.game_display)
